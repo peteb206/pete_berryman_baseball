67d9d18f62fdc9f85229bcc87725de2e9b5f6b76
Pete Berryman <peteb206@gmail.com>
2021-03-09 08:47:58 -0500

    Remove dependency on css file
    Remove dependency on css file

--- Projects/Canadian_Baseball_Network/roster_scrape.py
+++ Projects/Canadian_Baseball_Network/roster_scrape.py
@@ -394,36 +394,40 @@ def format_df(dict_list, schools_df):
 def generate_html(df, file_name, last_run):
     pd.set_option('colheader_justify', 'center')
 
-    datatables_function = '''
-         $(document).ready(function () {
-            $('table.display').DataTable({
-               'lengthMenu': [[10, 25, 50, 100, -1], [10, 25, 50, 100, 'All']],
-               'pageLength': -1,
-               'columnDefs': [{'targets': [ 0 ], 'visible': false, 'searchable': false}]
-            })
-         });
-    '''
-
     html_string = '''
-    <!DOCTYPE HTML>
-    <html>
-       <head>
-          <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-          <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
-          <link rel="stylesheet" type="text/css" href="df_style.css"/>
-          <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
-          <script type="text/javascript">
-             {datatables_function}
-          </script>
-          <title>2021 Canadians in College</title>
-       </head>
-       <header>
-          <h1>2021 Canadians in College</h1>
-          <h2>{number_of_players} Canadian players</h2>
-          <h5>{last_run}</h5>
-       </header>
-       <body>
-    '''.format(datatables_function = datatables_function, number_of_players = str(len(df.index)), last_run = last_run)
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {{
+                $('table.display').DataTable({{
+                    'lengthMenu': [[10, 25, 50, 100, -1], [10, 25, 50, 100, 'All']],
+                    'pageLength': -1,
+                    'order': [[2, 'asc'], [3, 'asc']],
+                    'dom': 'Bflrtip',
+                    'buttons': ['excel']
+                }})
+            }});
+        </script>
+        <title>2021 Canadians in College</title>
+    </head>
+    <header>
+        <h1 style='text-align: center'>2021 Canadians in College</h1>
+        <h2 style='text-align: center'>{number_of_players} Canadian players</h2>
+        <h5 style='text-align: center'>{last_run}</h5>
+    </header>
+    <body>
+    '''.format(number_of_players = str(len(df.index)), last_run = last_run)
 
     iter = 0
     for division in [
@@ -437,21 +441,26 @@ def generate_html(df, file_name, last_run):
     ]:
         temp_df = df[df['division'] == division].drop(['division'], axis=1)
         html_string += '''
-        <div class="mystyle">
-           <h2>{division}</h2>
-           {table}
+        <div style='padding:3%'>
+            <h2 style='text-align: center'>{division}</h2>
+            {table}
         </div>
         '''.format(division = division,
                    number_of_players = str(len(temp_df.index)),
-                   table = temp_df.to_html(na_rep = '', classes='display mystyle" id="table{}'.format(str(iter))))
+                   table = temp_df.to_html(na_rep = '', index=False, classes='display" id="table{}'.format(str(iter))))
         iter += 1
 
     html_string += '''
-       </body>
-    </html>.
+    </body>
+</html>.
     '''
 
+    # Make cells editable
     html_string = html_string.replace('<td>', "<td contenteditable='true'>")
+
+    # Custom sort order for class: Freshman, Sophomore, Junior, Senior
+    for class_year, class_year_num in {'Freshman': '1', 'Sophomore': '2', 'Junior': '3', 'Senior': '4'}.items():
+        html_string = html_string.replace('>{}<'.format(class_year), " data-order='{}'>{}<".format(class_year_num, class_year))
 
     with open(file_name, 'w') as f:
         f.write(html_string)
