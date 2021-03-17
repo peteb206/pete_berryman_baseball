a343db91acdd8b64d1fcb8434f6615461e6df5e4
Pete Berryman <peteb206@gmail.com>
2021-03-17 13:48:02 -0400

    consolidate secrets

--- Projects/Canadian_Baseball_Network/roster_scrape.py
+++ Projects/Canadian_Baseball_Network/roster_scrape.py
@@ -531,24 +531,7 @@ def update_gsheet(df, last_run):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
     # add credentials to the account
-    variables_keys = {
-        'type': os.environ.get('SHEET_TYPE'),
-        'project_id': os.environ.get('SHEET_PROJECT_ID'),
-        'private_key_id': os.environ.get('SHEET_PRIVATE_KEY_ID'),
-        'private_key': os.environ.get('SHEET_PRIVATE_KEY'),
-        'private_key': os.environ.get('SHEET_PRIVATE_KEY').replace('\\n', '\n'),
-        'client_email': os.environ.get('SHEET_CLIENT_EMAIL'),
-        'client_id': os.environ.get('SHEET_CLIENT_ID'),
-        'auth_uri': os.environ.get('SHEET_AUTH_URI'),
-        'token_uri': os.environ.get('SHEET_TOKEN_URI'),
-        'auth_provider_x509_cert_url': os.environ.get('SHEET_AUTH_PROVIDER_X509_CERT_URL'),
-        'client_x509_cert_url': os.environ.get('SHEET_CLIENT_X509_CERT_URL')
-    }
-    logger.info('')
-    for key, item in variables_keys.items():
-        logger.info('length of {}: {}'.format(key, str(len(item))))
-    logger.info('')
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(variables_keys, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ.get('KEYFILE')), scope)
 
     # authorize the clientsheet 
     client = gspread.authorize(creds)
